a7c789cd8da54a1b199e5116d6ff3b0f29ecbafc
CorbinGraves <corbingraves90@gmail.com>
2020-04-29 14:27:57 -0400

    Initial Commit

---
+++ manyHands/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  manyHands
+//
+//  Created by corbin graves on 4/29/20.
+//  Copyright © 2020 corbin graves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
